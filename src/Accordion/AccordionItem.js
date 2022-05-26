76f52bab99d8ed00df6407687f57a937d4d97431
Susmithag657 <susmithagullipalli2013@gmail.com>
2022-05-26 13:11:08 +0530

    2 files modified

--- src/Accordion/AccordionItem.js
+++ src/Accordion/AccordionItem.js
@@ -1,12 +1,18 @@
-import React,{useState} from 'react'
-const AccordionItem=({faq,answer})=>{
-  console.log(faq)
-  console.log(answer)
-return (
-  <div>
-    <div>{faq}<span> + </span></div>
-    <div>{answer}</div>
+import React, { useState } from 'react';
+const AccordionItem = ({ faq, answer }) => {
+  const [display, setdisplay] = useState(false);
+  const handleClick = (e) => {
+    e.preventDefault();
+    setdisplay(prevState=>!prevState)
+  };
+  return (
+    <div className="container">
+      <div className="faq">
+        {faq}
+        <button onClick={(e) => handleClick(e)}>{display ? '-' : '+'} </button>
+      </div>
+      {display? <div className="answer">{answer}</div> : null}
     </div>
-)
-}
-export default AccordionItem
+  );
+};
+export default AccordionItem;
